597398a8b796bccf0c916f3dbe8d6b9ff8642965
Dmitriy <dimon4ik_228@bk.ru>
2023-07-23 20:28:23 +0300

    delete database from settings

--- tg/tg/settings.py
+++ tg/tg/settings.py
@@ -99,12 +99,7 @@ WSGI_APPLICATION = 'tg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { }
 
 
 # Password validation
