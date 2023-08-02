1fbcf0b9680dd9801c0eccce81e92bbcf2b811be
Dmitriy <dimon4ik_228@bk.ru>
2023-08-02 22:07:37 +0300

    sender update rabbitmq

--- sender/sender.py
+++ sender/sender.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import os
 
 import asyncio
+import functools
+import json
+import pika
 import time
 
 from telethon import TelegramClient, events, Button, types, utils
@@ -21,6 +24,9 @@ FOLDER_DOWNLOADS = BASE_DIR / 'downloads'
 async def ticker(app):
     while True:
         for file in os.listdir(FOLDER_QUEUE):
+            if not file.endswith('.json'):
+                continue
+
             jsona = jsn.Jsona(FOLDER_QUEUE, file)
 
             data = jsona.return_json().get('data', {})
@@ -60,6 +66,9 @@ async def ticker(app):
                         os.remove(FOLDER_QUEUE / file)
                     except Exception as e:
                         print(e)
+            
+            except errors.rpcerrorlist.FloodWaitError as e:
+                print(f'Need wait {e.seconds}')
 
             except Exception as e:
                 print(e)
@@ -87,22 +96,164 @@ async def ticker(app):
         await asyncio.sleep(1)
 
 
+async def send_message(
+    app: TelegramClient,
+    data: dict,
+) -> dict:
+    now_time = int(time.time())
+
+    message_id = data.get('id', 'unknown')
+
+    if data.get('send_time', now_time) > now_time:
+        print(f'AWAIT QUEUE TIME {message_id}')
+
+        return {
+            "success": False,
+            "code": "error.send_time_retry",
+            "dt": data.get('send_time', now_time),
+            "error": "Need wait for send that message",
+        }
+
+    if not (
+        data.get('sender') and (
+            data.get('text') or 
+            (
+                data.get('file') and 
+                os.path.exists(FOLDER_DOWNLOADS / data['file']) 
+            )
+        )
+    ):
+        {
+            "success": False,
+            "code": "error.notcorrectfields",
+            "error": "No sender or one field of [text,file]",
+        }
+
+    try:
+        result = await app.send_message(
+            entity = data.get('sender'), 
+            message = data.get('text', ''),
+            reply_to = data.get('reply_to'),
+            file = FOLDER_DOWNLOADS / data['file'] if data.get('file') else None,
+            force_document = data.get('force_document', False),
+            link_preview = data.get('preview', False),
+            parse_mode = data.get('parse_mode', 'HTML'),
+        )
+
+        if result.id:
+            try:
+                if data.get('file') and not data.get('not_remove_file'):
+                    os.remove(FOLDER_DOWNLOADS / data['file'])
+                
+            except Exception as e:
+                print(e)
+    
+    except errors.rpcerrorlist.FloodWaitError as e:
+        print(f'Need wait {e.seconds} for {message_id}')
+
+        return {
+            "success": False,
+            "code": "error.floodwait",
+            "error": f"Flood wait {e.seconds} seconds for sending this message",
+            "dt": (int(time.time()) + e.seconds),
+        }
+
+    except Exception as e:
+        print(f'Unknown exception for {message_id}')
+
+        return {
+            "success": False,
+            "code": "error.unknown",
+            "error": type(e),
+        }
+
+    return {
+        "success": True,
+        "message_id": result.id if result else None,
+        "dt": int(time.time()),
+    }
+
+
 async def main():
-    print(FOLDER_QUEUE)
-
     jsona = jsn.Jsona(name_file='settings.json', path_file=BASE_DIR / 'sender')
-
     login_data = jsona.return_json().get('data', {})
 
+    jsona = jsn.Jsona(path_file=BASE_DIR, name_file='settings_rabbitmq.json')
+    rabbit_config = jsona.return_json().get('data', {})
+
+
     app = TelegramClient(
-        session = str(BASE_DIR / 'sender/sessions' / login_data['token'].split(':')[0]),
+        session = str(
+            BASE_DIR / 'sender/sessions' / login_data['token'].split(':')[0]
+        ),
         api_id = login_data['api_id'],
         api_hash = login_data['api_hash'],
+        flood_sleep_threshold = 0,
     )
     
     await app.start(
         bot_token = login_data['token'],
     )
+
+    connection = pika.BlockingConnection(
+        parameters = pika.URLParameters(
+            'amqp://{user}:{password}@{host}:{port}/%2F'.format(
+                **rabbit_config
+            )
+        )
+    )
+
+    channel = connection.channel()
+
+
+    async def create_consumer():
+        print('[X] Created consumer')
+
+        while True:
+            method_frame, header_frame, body = channel.basic_get(
+                queue = 'message',
+                auto_ack = False,
+            )
+
+            if method_frame:
+                try:
+                    data = json.loads(body)
+                except json.decoder.JSONDecodeError:
+                    data = body
+
+                print(f" [x] Received {data}")
+
+                result = await send_message(
+                    app = app,
+                    data = data,
+                )
+
+                if result.get('success'):
+                    print(f'success sended {data}')
+
+                elif result.get('code') in [
+                    'error.send_time_retry',
+                    'error.floodwait',
+                ]:
+                    data['send_time'] = result.get('dt', int(time.time()))
+
+                    channel.basic_publish(
+                        exchange = 'message',
+                        routing_key = 'message',
+                        body = json.dumps(
+                            obj = data,
+                        ),
+                    )
+
+                else:
+                    print(f'Unknown error is {result}')
+
+                channel.basic_ack(method_frame.delivery_tag)
+            
+            else:
+                print('[A] AWAIT')
+
+                await asyncio.sleep(1)
 
 
     @app.on(
@@ -128,7 +279,9 @@ async def main():
         )
     
 
-    asyncio.get_event_loop().create_task(ticker(app))
+    asyncio.create_task(
+        create_consumer()
+    )
 
     await app.run_until_disconnected()
 
